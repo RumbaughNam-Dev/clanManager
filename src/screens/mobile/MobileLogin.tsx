a3a7c0315374536735e89e0ee041fe7cf397d758
RumBaughNam <expoool@me.com>
2025-10-28 00:52:02 +0900

    모바일 로그인화면 추가

--- src/screens/mobile/MobileLogin.tsx
+++ src/screens/mobile/MobileLogin.tsx
@@ -1,6 +1,6 @@
 // src/screens/mobile/MobileLogin.tsx
-import React from "react";
 import Login from "../Auth/Login";
+import "./mobile-login.css";
 
 type Props = {
   onGoSignup: () => void;
@@ -8,9 +8,9 @@ type Props = {
 
 export default function MobileLogin({ onGoSignup }: Props) {
   return (
-    <div className="fixed inset-0 bg-black text-white">
-      <div className="absolute inset-[5%]">
-        <div className="w-full h-full">
+    <div className="mobile-login fixed inset-0 bg-black text-white">
+      <div className="absolute inset-[5%] flex items-center justify-center">
+        <div className="w-full max-w-[520px]">
           <Login onGoSignup={onGoSignup} />
         </div>
       </div>
